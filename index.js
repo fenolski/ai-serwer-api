25e9e265d0be73ef258b14dca407d6a1f83089c7
fenolski <fenolski@gmail.com>
2022-12-08 14:06:58 +0100

    Dodane pull i get

--- index.js
+++ index.js
@@ -1,7 +1,30 @@
 const express = require("express");
+const { createImportSpecifier } = require("typescript");
 const app = express();
 
 app.listen(8888, () => {
     console.log("App started");
 })
+
+app.get('/', (req,res) => {
+        console.log(req.body)
+        res.send("serwer działa")
+    }
+)
+
+app.post('/', (req, res) => {
+        console.log(req.body)
+        res.status(200).end()
+    }
+)
+
+// app.post('/', (req, res) => {
+//     //miejsce na kod uruchunieniowy AI pyhona
+//     const { exec } = require("childprocess");
+
+//     exec("ls -la", (err, stdout, stderr) => {
+//         console.log('Error triggered')
+//     }); 
+//     res.send("Has sent!")
+// })
 
