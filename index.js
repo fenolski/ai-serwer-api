d312e9279edb9d869dc38af9feaf70d5faea1066
fenolski <fenolski@gmail.com>
2022-12-08 14:42:55 +0100

    Exec + CORS

--- index.js
+++ index.js
@@ -1,30 +1,28 @@
 const express = require("express");
 const { createImportSpecifier } = require("typescript");
+const cors = require("cors")
+const {exec} = require("child_process")
 const app = express();
+
+app.use(cors())
 
 app.listen(8888, () => {
     console.log("App started");
 })
 
 app.get('/', (req,res) => {
-        console.log(req.body)
-        res.send("serwer działa")
+        res.send("App get")
+        exec("ls -la", () => {
+            console.log("Exec triggered")
+            })        
     }
 )
 
 app.post('/', (req, res) => {
-        console.log(req.body)
+        res.send("App post")
+        exec("ls -la", () => {
+            console.log("Exec triggered")
+            })
         res.status(200).end()
     }
 )
-
-// app.post('/', (req, res) => {
-//     //miejsce na kod uruchunieniowy AI pyhona
-//     const { exec } = require("childprocess");
-
-//     exec("ls -la", (err, stdout, stderr) => {
-//         console.log('Error triggered')
-//     }); 
-//     res.send("Has sent!")
-// })
-
